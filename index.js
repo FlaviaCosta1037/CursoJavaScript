0d96cde9e46ccdf2398e09ba4767522375186757
FlaviaCo <https://github.com/FlaviaCosta1037/estudosJava.git>
2022-02-15 16:53:42 -0300

    Aprendendo console.log

---
+++ index.js
@@ -0,0 +1,13 @@
+//Função console.log
+//Obs O ; é opicional no JS. Recomenda-se utilizar.
+// Sempre que usar essa função seguida, ela quebra a linha.
+// Estruturas disponíveis para utilização de aspas simples, duplas e crase.
+
+console.log("Flávia 'Costa'"); 
+console.log('Flávia "Costa"'); 
+console.log(`"Flávia" 'Costa'`);
+
+console.log(123456, 15.85); // exibir numeros inteiros, reais)
+
+console.log(123456, 15.85, 'Flávia Costa'); // exibir numeros inteiros, reais e strings)
+
