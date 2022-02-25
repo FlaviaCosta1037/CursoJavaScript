7ea06bdde8582b414c89058c77209e1e4878eba7
Flavia Costa <flavinha.goncalves@gmail.com>
2022-02-24 21:52:35 -0300

    Convertendo string em numero.

---
+++ aula_11/js/script.js
@@ -0,0 +1,9 @@
+let numero1 =  prompt('Informe um valor: ');
+let numero2 =  prompt('Informe um valor: ');
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+let resultado = numero1 + numero2;
+
+alert(`O resultado da soma dos resultados é: ${resultado} `);
