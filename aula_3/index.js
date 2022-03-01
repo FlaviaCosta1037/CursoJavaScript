e673ca523bb3437a9427d85b9ed6cb2fcfe26d34
Flavia Costa <flavinha.goncalves@gmail.com>
2022-03-01 13:29:37 -0300

    Informação sobre String (caracter de escape)

--- aula_3/index.js
+++ aula_3/index.js
@@ -6,8 +6,12 @@
 console.log("Flávia 'Costa'"); 
 console.log('Flávia "Costa"'); 
 console.log(`"Flávia" 'Costa'`);
-
 console.log(123456, 15.85); // exibir numeros inteiros, reais)
 
 console.log(123456, 15.85, 'Flávia Costa'); // exibir numeros inteiros, reais e strings)
+
+//Forma não usual
+
+let umaString = "Um \"texto\""; //Barra invertida como caracter de escape.
+console.log(umaString);
 
